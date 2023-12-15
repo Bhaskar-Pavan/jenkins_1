fe1c1405f758e27abb1d97c8dca2028c3aea9aea
Bhaskar-Pavan <153126965+Bhaskar-Pavan@users.noreply.github.com>
2023-12-15 15:05:08 +0530

    Update EvenNumbers.java

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -2,8 +2,8 @@ public class EvenNumbers {
     public static void main(String[] args) {
         // Loop to print the first 10 even numbers
         for (int i = 1; i <= 20; i++) {
-            if (i % 2 != 0) {
-                System.out.println(i);
+            if (i % 2 == 0) {
+                System.out.println("hi"+i);
             }
         }
     }
