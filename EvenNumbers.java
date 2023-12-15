fa70b6b1ff0ce14306147a5a71f63d71c614004c
Bhaskar-Pavan <153126965+Bhaskar-Pavan@users.noreply.github.com>
2023-12-15 15:02:24 +0530

    Update EvenNumbers.java

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -2,7 +2,7 @@ public class EvenNumbers {
     public static void main(String[] args) {
         // Loop to print the first 10 even numbers
         for (int i = 1; i <= 20; i++) {
-            if (i % 2 == 0) {
+            if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
